5b00056027fa8e1e32828a924a02319a9411448a
bharat4666 <102464588+bharat4666@users.noreply.github.com>
2025-08-24 12:24:16 +0000

    memory

---
+++ basics/memory.js
@@ -0,0 +1,21 @@
+let name1 = "Bharat"
+
+let name2 = name1
+
+name2 = "Aviral"
+console.log(name2);
+console.log(name1);
+
+let myobj = {
+    email : "bharat@gmail.com",
+    id : 2
+}
+console.log(myobj.email);
+
+let newObj = myobj
+
+newObj.email = "Aviral@gmail.com"
+ console.log(myobj.email);
+ console.log(newObj.email);
+ 
+ 
