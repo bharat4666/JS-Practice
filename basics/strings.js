13d6c7c239492188b744aa80a83e320ed4b6f51d
bharat4666 <102464588+bharat4666@users.noreply.github.com>
2025-08-24 13:11:15 +0000

    string methods

---
+++ basics/strings.js
@@ -0,0 +1,30 @@
+let name = "Bharat"
+let score = 5
+console.log(`My name is ${name} and my score is ${score}` );
+
+
+let gameName = new String('Bharat')
+
+console.log(gameName[0]);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf("t"));
+
+console.log(gameName.substring(0,3));
+
+console.log(gameName.slice(-6,3));
+
+let newString = "   Candy   "
+console.log(newString.trim());
+
+let ntid = "HHi4nn%&fk"
+console.log(ntid.replace('%&','Bharat'));
+
+console.log(ntid.includes('%&'));
+
+
