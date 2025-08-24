9bad62c5ee13010e1b2841e86e4b6336a936bb23
bharat4666 <102464588+bharat4666@users.noreply.github.com>
2025-08-24 11:44:32 +0000

    comparison and conversion

---
+++ basics/comparison.js
@@ -0,0 +1,23 @@
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+
+console.log("2" > 1);
+// Operator converts string into number
+
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null > 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+console.log(0 === 0);
+
+
+
+
+
+
--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -24,3 +24,32 @@ console.log(booleanisLoggedin);
 let b = 7
 b = "HI"
 console.log(typeof b);
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2*2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Bharat"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);       
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
